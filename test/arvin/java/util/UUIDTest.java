818e4d1e5265953d6aa84a6c790aef9aa99bdf90
Alvin <wrjcode@163.com>
2023-04-21 17:56:54 +0800

    update test package

--- test/arvin/java/util/UUIDTest.java
+++ test/arvin/java/util/UUIDTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UUIDTest {
@@ -18,10 +20,27 @@ class UUIDTest {
 
     @Test
     void randomUUID() {
+        UUID uuid = UUID.randomUUID();
+        System.out.println(uuid);
     }
 
     @Test
     void nameUUIDFromBytes() {
+        byte[] bytes = new byte[10];
+        bytes[0] = 96;
+        bytes[1] = 98;
+        bytes[2] = 102;
+        bytes[3] = 112;
+        bytes[4] = 100;
+        bytes[5] = 96;
+        bytes[6] = 98;
+        bytes[7] = 102;
+        bytes[8] = 112;
+        bytes[9] = 100;
+
+        UUID uuid = UUID.nameUUIDFromBytes(bytes);
+
+        System.out.println(uuid);
     }
 
     @Test
